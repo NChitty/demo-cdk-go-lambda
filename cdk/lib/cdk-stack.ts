1366bd3f944b581b80a1bf13fb780421eb70c165
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-07-03 13:29:26 -0400

    Finished with cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,42 @@
-import * as cdk from 'aws-cdk-lib';
+import {CfnOutput, Duration, Stack, StackProps} from 'aws-cdk-lib';
+import { Code, Function, FunctionUrlAuthType, Handler, HttpMethod, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import path = require('path');
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const demoLambda = new Function(this, 'LambdaFunction', {
+      functionName: 'GoFunction',
+      runtime: Runtime.FROM_IMAGE,
+      code: Code.fromAssetImage(path.join(__dirname, '..', '..', 'lambda'), {
+        assetName: 'go-lambda',
+      }),
+      handler: Handler.FROM_IMAGE,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaFunctionUrl = demoLambda.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ['*'],
+        allowedHeaders: ['Authorization'],
+        allowedMethods: [
+          HttpMethod.ALL,
+          // HttpMethod.GET,
+          // HttpMethod.HEAD,
+          // HttpMethod.OPTIONS,
+          // HttpMethod.POST,
+          // HttpMethod.DELETE,
+          // HttpMethod.PUT,
+          // HttpMethod.PATCH,
+        ],
+        maxAge: Duration.days(1),
+      }
+    });
+
+    new CfnOutput(this, 'LambdaUrl', {
+      value: lambdaFunctionUrl.url
+    });
   }
 }
